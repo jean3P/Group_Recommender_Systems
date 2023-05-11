7d28902b9e3c8ef42c341dff6d28643544c1f3f5
Jean 2P Principe <jean.pereyraprincipe@students.unibe.ch>
2023-05-11 13:58:29 +0200

    Feat: First iteration

--- src/Helper.py
+++ src/Helper.py
@@ -1,52 +1,12 @@
-# import pandas as pd
-#
-# # Load the data
-# movies_vector_xlsx = pd.read_excel('../resources/GRS_dataset.xlsx', sheet_name='Movies Vector Representation')
-# users_movie_ratings = pd.read_excel('../resources/GRS_dataset.xlsx', sheet_name='Users-Movie Ratings')
-#
-# # Eliminar la primera fila y columna
-# rating_matrix_without_average = users_movie_ratings.iloc[0:50, 0:]
-# rating_matrix_with_average = users_movie_ratings.iloc[54:, 0:]
-#
-# movies_to_recommend = movies_vector_xlsx.iloc[0:15, 1:]
-# movies_to_recommend_selected = movies_to_recommend.iloc[:, 0:2]
-# movies_to_recommend_selected['Id Number'] = movies_to_recommend_selected['Id Number'].astype(int)
-# movies_rated_by_user = movies_vector_xlsx.iloc[16:, 1:]
-#
-# similarity_Rated_Movies_newMov_xlsx = pd.read_excel('../resources/GRS_dataset.xlsx', sheet_name='Similarity RatedMovies & NewMov')
-#
-# cos_similarity_between_moviesRat_and_moviesReco = similarity_Rated_Movies_newMov_xlsx.iloc[1:, :]
-# # Asumiendo que tu DataFrame es cos_similarity_between_moviesRat_and_moviesReco
-# df = cos_similarity_between_moviesRat_and_moviesReco
-#
-# # Filtra las columnas que contienen las keys (títulos)
-# df = df.rename(columns={'COSINE SIMILARITY between the rated movies and the movies to be recommended | Most similar to least similar order': 'Unnamed:0'})
-# keys_columns = [col for col_idx, col in enumerate(df.columns) if col_idx % 2 == 0]
-#
-# # Crea un diccionario vacío para almacenar los DataFrames
-# dataframes_dict = {}
-#
-# # Itera sobre las columnas de keys y crea un DataFrame para cada key
-# for key_col_idx, key_col in enumerate(keys_columns):
-#     # Obtiene la key (título)
-#     key = df.iloc[0][key_col]
-#
-#     # Encuentra la columna de ratings correspondiente a la key
-#     ratings_col = df.columns[key_col_idx * 2 + 1]
-#
-#     # Elimina la primera fila (la que contiene las keys) y la columna de NaNs
-#     sub_df = df[[key_col, ratings_col]].drop(1)
-#     sub_df = sub_df.dropna()
-#
-#     # Renombra las columnas para que sean consistentes en todos los DataFrames
-#     sub_df.columns = ['Title', 'Rating']
-#
-#     # Añade el DataFrame al diccionario utilizando la key
-#     dataframes_dict[key] = sub_df
-
-# print(rating_matrix_with_average)
-
 # Choose two groups from the Table 1
+
+last_misery_agg = 'Last Misery'
+most_pleasure_agg = 'Most Pleasure'
+average = 'Average'
+
+GRS_dataset = '../resources/GRS_dataset.xlsx'
+title_sim = 'COSINE SIMILARITY between the rated movies and the movies to be recommended | Most similar to least similar order'
+
 table_1 = {
     1: ['U10', 'U26', 'U30', 'U12', 'U11', 'U16', 'U37', 'U29', 'U36'],
     2: ['U2', 'U3', 'U28', 'U44', 'U41'],
@@ -82,98 +42,3 @@ table_3_dict = {
     14: [6, 5, 14],
     15: [13, 5, 6]
 }
-
-
-#
-# def calculate_aggregations(users, user_movie_ratings):
-#     group_ratings = user_movie_ratings[users]
-#     group_ratings = group_ratings.T
-#     # Apply aggregation strategies
-#     avg_ratings = group_ratings.mean(axis=0)
-#     min_ratings = group_ratings.min(axis=0)
-#     max_ratings = group_ratings.max(axis=0)
-#
-#     return avg_ratings, min_ratings, max_ratings
-#
-#
-# def construct_matrix_with_aggregations(users, user_movie_ratings):
-#     avg_ratings, min_ratings, max_ratings = calculate_aggregations(users, user_movie_ratings)
-#     # user_movie_ratings[f'Average'] = avg_ratings
-#     user_movie_ratings[f'Last Misery'] = min_ratings
-#     # user_movie_ratings[f'Most Pleasure'] = max_ratings
-#
-#     return user_movie_ratings
-#
-# # Select two groups
-# selected_groups = [2]
-#
-# # Eliminar las dos primeras columnas de rating_matrix_with_average
-# column_name = rating_matrix_with_average.columns[2]
-# user_movie_ratings = rating_matrix_with_average.drop(columns=column_name, axis=1)
-#
-# for s_g in selected_groups:
-#     final = construct_matrix_with_aggregations(table_1[s_g], user_movie_ratings)
-#
-# # Asumiendo que tu DataFrame es df
-# sorted_df = final.sort_values(by='Last Misery', ascending=False)
-# selected_columns_Last_Misery = sorted_df.iloc[:7, [0, 1, -1]]
-#
-# # Crea una lista vacía para almacenar las películas más similares
-# # Crea un diccionario vacío para almacenar las películas más similares
-# most_similar_movies_dict = {}
-#
-# # Itera sobre las filas del DataFrame selected_columns_Last_Misery
-# for index, row in selected_columns_Last_Misery.iterrows():
-#     # Obtiene el título de la película de la columna 2
-#     movie_title = row.iloc[1]
-#
-#     # Encuentra el DataFrame correspondiente en dataframes_dict utilizando la key (título de la película)
-#     movie_df = dataframes_dict[movie_title]
-#
-#     # Encuentra el rating más alto
-#     highest_rating = movie_df['Rating'].max()
-#
-#     # Si el rating más alto es mayor que 0, encuentra todas las películas con ese rating
-#     if highest_rating > 0:
-#         highest_rated_movies = movie_df[movie_df['Rating'] == highest_rating]
-#
-#         # Añade el DataFrame de películas más similares al diccionario usando el título de la película como clave
-#         most_similar_movies_dict[movie_title] = highest_rated_movies
-#
-#
-# # Convierte la lista de películas más similares en un DataFrame
-# # most_similar_movies_df = pd.DataFrame(most_similar_movies, columns=['Most Similar Movie', 'Rating'])
-# # Añade las columnas del DataFrame most_similar_movies_df a selected_columns_df
-# selected_columns_Last_Misery.reset_index(drop=True, inplace=True)
-#
-# rating_multiplication_dataframes = []
-#
-# # Itera sobre las filas del DataFrame selected_columns_Last_Misery
-# for index, row in selected_columns_Last_Misery.iterrows():
-#     # Obtiene el título de la película de la columna 2
-#     movie_title = row.iloc[1]
-#
-#     # Obtiene el rating de la película
-#     movie_rating = row.iloc[-1]
-#
-#     # Encuentra el DataFrame de películas más similares en el diccionario utilizando el título de la película como clave
-#     similar_movies_df = most_similar_movies_dict.get(movie_title)
-#
-#     # Si el DataFrame de películas más similares existe, multiplica el rating de la película por el rating de cada película similar
-#     if similar_movies_df is not None:
-#         # Crea una copia del DataFrame antes de modificarlo
-#         similar_movies_df = similar_movies_df.copy()
-#         similar_movies_df['Ranked List Recommendation'] = similar_movies_df['Rating'] * movie_rating
-#
-#         # Añade el DataFrame con los resultados de la multiplicación a la lista
-#         rating_multiplication_dataframes.append(similar_movies_df)
-#
-# # Combina todos los DataFrames de la lista en un solo DataFrame
-# resulting_dataframe = pd.concat(rating_multiplication_dataframes, ignore_index=True)
-#
-# # Agrupa el DataFrame por la columna "Title" y encuentra el índice con el valor más alto en la columna "Ranked List Recommendation"
-# max_ranked_list_recommendation_idx = resulting_dataframe.groupby('Title')['Ranked List Recommendation'].idxmax()
-#
-# # Filtra el DataFrame utilizando los índices encontrados y crea un nuevo DataFrame sin títulos duplicados
-# unique_movies_with_highest_ranked_list_recommendation = resulting_dataframe.loc[max_ranked_list_recommendation_idx].reset_index(drop=True)
-# unique_movies_with_highest_ranked_list_recommendation = unique_movies_with_highest_ranked_list_recommendation.sort_values(by='Ranked List Recommendation', ascending=False)
--- src/Main_2.py
+++ src/Main_2.py
@@ -1,23 +1,28 @@
 import pandas as pd
 
-from Helper import table_1, table_3_dict
-
-last_misery_agg = 'Last Misery'
-most_pleasure_agg = 'Most Pleasure'
-average = 'Average'
+from Helper import table_1, table_3_dict, average, last_misery_agg, most_pleasure_agg, GRS_dataset, title_sim
 
 
+# Definir constantes para los nombres de las estrategias de agregación
+
+
+# Función para cargar los datos del archivo excel
 def load_data():
-    movies_vector_xlsx = pd.read_excel('../resources/GRS_dataset.xlsx', sheet_name='Movies Vector Representation')
-    users_movie_ratings = pd.read_excel('../resources/GRS_dataset.xlsx', sheet_name='Users-Movie Ratings')
+    # Cargar las dos hojas del archivo excel en dataframes de pandas
+    movies_vector_xlsx = pd.read_excel(GRS_dataset, sheet_name='Movies Vector Representation')
+    users_movie_ratings = pd.read_excel(GRS_dataset, sheet_name='Users-Movie Ratings')
 
+    # Dividir el dataframe de calificaciones de los usuarios en dos partes: sin y con la fila de promedios
     rating_matrix_without_average = users_movie_ratings.iloc[0:50, 0:]
     rating_matrix_with_average = users_movie_ratings.iloc[54:, 0:]
 
     return movies_vector_xlsx, rating_matrix_without_average, rating_matrix_with_average
 
 
+# Función para preparar los dataframes que se utilizarán en el análisis
 def prepare_dataframes(movies_vector_xlsx):
+    # Separar el dataframe de vectores de películas en dos partes:
+    # las películas a recomendar y las películas calificadas por el usuario
     movies_to_recommend = movies_vector_xlsx.iloc[0:15, 1:]
     movies_to_recommend_selected = movies_to_recommend.iloc[:, 0:2]
     movies_to_recommend_selected['Id Number'] = movies_to_recommend_selected['Id Number'].astype(int)
@@ -26,88 +31,139 @@ def prepare_dataframes(movies_vector_xlsx):
     return movies_to_recommend_selected, movies_rated_by_user
 
 
+# Función para cargar los datos de similitud del archivo excel
 def load_similarity_data():
-    similarity_Rated_Movies_newMov_xlsx = pd.read_excel('../resources/GRS_dataset.xlsx',
+    # Cargar la hoja de similitud del archivo excel en un dataframe de pandas
+    similarity_Rated_Movies_newMov_xlsx = pd.read_excel(GRS_dataset,
                                                         sheet_name='Similarity RatedMovies & NewMov')
+
+    # Eliminar la primera fila del dataframe
     cos_similarity_between_moviesRat_and_moviesReco = similarity_Rated_Movies_newMov_xlsx.iloc[1:, :]
 
     return cos_similarity_between_moviesRat_and_moviesReco
 
 
-def get_dataframes_dict(df):
-    df = df.rename(columns={
-        'COSINE SIMILARITY between the rated movies and the movies to be recommended | Most similar to least similar order': 'Unnamed:0'})
+def get_dataframes_dict_of_similarities(df):
+    # Renombrar la columna del título de la película en el dataframe para un manejo más sencillo
+    df = df.rename(columns={title_sim: 'Unnamed:0'})
+
+    # Crear una lista con los nombres de las columnas con índice par en el dataframe
     keys_columns = [col for col_idx, col in enumerate(df.columns) if col_idx % 2 == 0]
 
+    # Inicializar un diccionario vacío que se llenará con los sub-dataframes
     dataframes_dict = {}
 
+    # Iterar sobre las columnas de las claves
     for key_col_idx, key_col in enumerate(keys_columns):
+        # Extraer el nombre de la película como clave del primer valor en la columna
         key = df.iloc[0][key_col]
 
+        # Identificar la columna de calificaciones que corresponde a la columna de claves actual
         ratings_col = df.columns[key_col_idx * 2 + 1]
 
+        # Crear un sub-dataframe con solo la columna de claves y la columna de calificaciones, y eliminar la primera fila
         sub_df = df[[key_col, ratings_col]].drop(1)
+
+        # Eliminar cualquier fila con un valor nulo
         sub_df = sub_df.dropna()
 
+        # Renombrar las columnas del sub-dataframe para un manejo más sencillo
         sub_df.columns = ['Title', 'Rating']
 
+        # Agregar el sub-dataframe al diccionario con la clave del título de la película
         dataframes_dict[key] = sub_df
 
+    # Devolver el diccionario de sub-dataframes
     return dataframes_dict
 
 
 def calculate_aggregations(users, user_movie_ratings):
+    # Seleccionar las calificaciones de las películas para el grupo de usuarios dado
     group_ratings = user_movie_ratings[users]
+
+    # Transponer el dataframe para que los usuarios sean las columnas y las películas sean las filas
     group_ratings = group_ratings.T
+
+    # Calcular la calificación promedio para cada película
     avg_ratings = group_ratings.mean(axis=0)
+
+    # Calcular la calificación mínima para cada película (Last misery)
     min_ratings = group_ratings.min(axis=0)
+
+    # Calcular la calificación máxima para cada película (Most pleasure)
     max_ratings = group_ratings.max(axis=0)
 
+    # Devolver las calificaciones promedio, mínimas y máximas
     return avg_ratings, min_ratings, max_ratings
 
 
 def construct_matrix_with_aggregations(users, user_movie_ratings):
+    # Calcular las calificaciones promedio, mínimas y máximas para el grupo de usuarios dado
     avg_ratings, min_ratings, max_ratings = calculate_aggregations(users, user_movie_ratings)
+
+    # Crear copias del dataframe original de calificaciones de películas para cada estrategia de agregación
     user_movie_ratings_avg = user_movie_ratings_lm = user_movie_ratings_mp = user_movie_ratings.copy()
+
+    # Agregar las calificaciones promedio al dataframe de calificaciones promedio
     user_movie_ratings_avg[average] = avg_ratings
+
+    # Agregar las calificaciones mínimas al dataframe de calificaciones mínimas
     user_movie_ratings_lm[last_misery_agg] = min_ratings
+
+    # Agregar las calificaciones máximas al dataframe de calificaciones máximas
     user_movie_ratings_mp[most_pleasure_agg] = max_ratings
 
+    # Devolver los dataframes de calificaciones para cada estrategia de agregación
     return user_movie_ratings_avg, user_movie_ratings_lm, user_movie_ratings_mp
 
 
 def get_most_similar_movies_dict(selected_columns_aggregation, dataframes_dict):
+    # Crear un diccionario para almacenar las películas más similares
     most_similar_movies_dict = {}
 
+    # Iterar sobre cada fila en la agregación seleccionada
     for index, row in selected_columns_aggregation.iterrows():
+        # Obtener el título de la película
         movie_title = row.iloc[1]
+
+        # Gestionar los títulos de las películas que son cadenas o no
         if type(movie_title) == str:
             movie_df = dataframes_dict[movie_title.strip('\n')]
         else:
             movie_df = dataframes_dict[movie_title]
 
+        # Obtener la calificación más alta en el dataframe de la película
         highest_rating = movie_df['Rating'].max()
 
+        # Si la calificación más alta es mayor que 0, añadir las películas con la calificación más alta al diccionario
         if highest_rating > 0:
             highest_rated_movies = movie_df[movie_df['Rating'] == highest_rating]
             most_similar_movies_dict[movie_title] = highest_rated_movies
 
+    # Devolver el diccionario de películas más similares
     return most_similar_movies_dict
 
 
 def get_rating_multiplication_dataframes(selected_columns_Last_Misery, most_similar_movies_dict):
+    # Crear una lista para almacenar los dataframes de multiplicación de calificaciones
     rating_multiplication_dataframes = []
 
+    # Iterar sobre cada fila en las columnas seleccionadas de la última miseria
     for index, row in selected_columns_Last_Misery.iterrows():
+        # Obtener el título de la película y la calificación
         movie_title = row.iloc[1]
         movie_rating = row.iloc[-1]
+
+        # Obtener el dataframe de películas similares para el título de la película
         similar_movies_df = most_similar_movies_dict.get(movie_title)
 
+        # Si se encuentra un dataframe de películas similares, multiplicar las calificaciones por la calificación de la película
         if similar_movies_df is not None:
             similar_movies_df = similar_movies_df.copy()
             similar_movies_df['Ranked List Recommendation'] = similar_movies_df['Rating'] * movie_rating
             rating_multiplication_dataframes.append(similar_movies_df)
 
+    # Devolver la lista de dataframes de multiplicación de calificaciones
     return rating_multiplication_dataframes
 
 
@@ -132,7 +188,7 @@ movies_to_recommend_selected, movies_rated_by_user = prepare_dataframes(movies_v
 cos_similarity_between_moviesRat_and_moviesReco = load_similarity_data()
 
 # Obtener diccionario de DataFrames
-dataframes_dict = get_dataframes_dict(cos_similarity_between_moviesRat_and_moviesReco)
+dataframes_dict = get_dataframes_dict_of_similarities(cos_similarity_between_moviesRat_and_moviesReco)
 
 
 def format_recommendations(recommendations, group_id, strategy):
@@ -181,7 +237,7 @@ def get_table_recommendations(user_movie_ratings_lm, user_movie_ratings_avg, use
 
 # Seleccionar dos grupos
 # selected_groups = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-selected_groups = [1, 5]
+selected_groups = [1, 3]
 # Eliminar las dos primeras columnas de rating_matrix_with_average
 column_name = rating_matrix_with_average.columns[2]
 user_movie_ratings = rating_matrix_with_average.drop(columns=column_name, axis=1)
@@ -215,7 +271,7 @@ def get_movie_titles_from_ids(movie_ids, movies_to_recommend_selected):
     movie_titles = []
     for movie_id in movie_ids:
         movie_title = \
-        movies_to_recommend_selected.loc[movies_to_recommend_selected['Id Number'] == movie_id, 'Title'].iloc[0]
+            movies_to_recommend_selected.loc[movies_to_recommend_selected['Id Number'] == movie_id, 'Title'].iloc[0]
         movie_titles.append(movie_title)
     return movie_titles
 
